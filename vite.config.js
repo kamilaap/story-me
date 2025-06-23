aa3d80184905b5f64655d219367c9a317561fa87
kamilaap <kp.herlambang@gmail.com>
2025-06-23 19:33:42 +0700

    let me fix again

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   root: resolve(__dirname, 'src'),
-  publicDir: resolve(__dirname, 'src', 'public'),
+  publicDir: resolve(__dirname, 'public'), 
   build: {
     outDir: resolve(__dirname, 'dist'),
     emptyOutDir: true,
